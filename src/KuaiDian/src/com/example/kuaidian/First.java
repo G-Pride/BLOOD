eeee073f4db9db50703126e5e208ed3ae1f93c0a
G-Pride <493536604@qq.com>
2016-12-12 11:49:15 +0800

    food message

--- src/KuaiDian/src/com/example/kuaidian/First.java
+++ src/KuaiDian/src/com/example/kuaidian/First.java
@@ -5,10 +5,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class First extends Activity{
 	private TextView usernameTextView;
+	private Button enterButton,exitButton;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.first);
@@ -20,6 +24,26 @@ public class First extends Activity{
 		usernameTextView = (TextView) findViewById(R.id.tV_userName);
 		Intent intent1 = getIntent();
 		usernameTextView.setText("" + intent1.getStringExtra("userName"));
+		exitButton = (Button) findViewById(R.id.bt_exit);
+		exitButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(First.this, MainActivity.class);
+				startActivity(intent);
+				First.this.finish();
+				
+			}
+		});
+		enterButton = (Button) findViewById(R.id.bt_enter);
+		enterButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(First.this, ShopCar.class);
+				startActivity(intent);
+				First.this.finish();
+				
+			}
+		});
 	}
 	
 	public boolean onOptionsItemSelected(MenuItem item) {
---
+++ src/KuaiDian/src/com/example/kuaidian/GroupInfo.java
@@ -0,0 +1,18 @@
+package com.example.kuaidian;
+
+
+
+public class GroupInfo extends BaseInfo
+{
+	public GroupInfo()
+	{
+		super();
+	}
+
+	public GroupInfo(String id, String name)
+	{
+		super(id, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
